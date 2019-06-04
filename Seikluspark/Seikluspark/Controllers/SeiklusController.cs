5914a86d9a9adb2427568bd44d5d774f4a8969cf
opilane <opilane@HP-CZC6297B8T>
2019-06-04 11:42:50 +0300

    tegin migrations ja kohandasin web.config.

--- Seikluspark/Seikluspark/Controllers/SeiklusController.cs
+++ Seikluspark/Seikluspark/Controllers/SeiklusController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using Seikluspark.Models;
 
 namespace Seikluspark.Controllers
 {
     public class SeiklusController : Controller
     {
+        private seiklusContext db = new seiklusContext();
         // GET: Seiklus
-        public ActionResult seikluspark()
+        public ActionResult Kohal()
         {
-            return View();
+            return View(db.Seikluspark.ToList());
         }
         public ActionResult ülevaade()
         {
--- Seikluspark/Seikluspark/Models/park.cs
+++ Seikluspark/Seikluspark/Models/park.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Seikluspark.Models
 {
     public class park
     {
+        public int Id { get; set; }
         public int inimesed { get; set; }
         public int käepaelad { get; set; }
     }
